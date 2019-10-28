a29c2625013fcae1f09867bb510ba755ba077094
Rish Shadra <rshadra@gmail.com>
2019-10-28 16:26:51 -0400

    Updated navigation

--- machine_usage/urls.py
+++ machine_usage/urls.py
@@ -4,4 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('our_space', views.index, name='our_space'),
+    path('equipment', views.index, name='equipment'),
+    path('status', views.index, name='status'),
+    path('news', views.index, name='news'),
+    path('hours', views.index, name='hours'),
+    path('login', views.index, name='login'),
 ]
