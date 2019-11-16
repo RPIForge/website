b6bde540843d5118212e4dfd535733b39293cd93
HonakerM <mchonaker@gmail.com>
2019-11-16 15:33:34 -0500

    This commit will change how emails are verified. Instead of having a attribute in the postgresql database I created a Verfied_Email group. If the user is in the group then their email is verified. I also created a verfied email template page. While it is not pretty it is functional. Finally I added the user profile into the admin page.

--- machine_usage/views.py
+++ machine_usage/views.py
@@ -1,7 +1,9 @@
+#import django methods
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User, Group
 
 #import database tables
 from machine_usage.models import UserProfile
@@ -47,23 +49,41 @@ def render_status(request):
     return render(request, 'machine_usage/index.html', {}) # TO-DO: Implement the "Status" page.
 
 def render_verify_email(request):
+    #output dictionary
+    verification_dict = {"already_verified":False,"email_verified":False}
+    
     #if method is correct type
     if request.method == 'GET':
         #get email id from request
-        email_token = request.GET.get("email_token",'')
+        token = request.GET.get("user_token",'')
         
         #if no email id paramaters redirect to error
-        if(email_token == ''):
-            print("failed 1")
-        
+        if(token == ''):
+            print("Failed 1")
+            return render(request, 'machine_usage/verify_email.html', verification_dict)
+            
         #try getting the user that corispondes to the token
         try:
-            user = UserProfile.objects.get(verification_token=email_token)
-            print(user)
+            #get user id from user profile which is from token
+            user_profile = UserProfile.objects.get(user_token=token)
+            user_id = user_profile.user_id
+            #get the user and group objects
+            user = User.objects.get(id=user_id)
+            group = Group.objects.get(name="Verified_Email")
+            
+            #if user is in group then return already verified
+            if(user.groups.filter(name="Verified_Email")):
+                verification_dict["already_verified"] = True
+                verification_dict["email_verified"] = True            
+            else:
+                #add user to email group
+                user.groups.add(group)
+                user.save()
+                verification_dict["email_verified"] = True
         except:
-            print("failed 2")
-
-    return redirect('/machine_usage/') 
+            verification_dict["email_verified"] = False
+            return render(request, 'machine_usage/verify_email.html',verification_dict)
+    return render(request, 'machine_usage/verify_email.html',verification_dict) 
          
        
 @login_required
@@ -73,4 +93,4 @@ def render_myforge(request):
 def log_out(request):
     logout(request)
     return redirect('/machine_usage/')
-    
+
