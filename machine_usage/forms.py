8657ba904a8979cb38581fec8f5546d0eae3c045
HonakerM <mchonaker@gmail.com>
2019-11-23 20:44:52 -0500

    Delete extra file and add check for duplicate emails
    This commit adds a check to make sure the user email is not already in the database

--- machine_usage/forms.py
+++ machine_usage/forms.py
@@ -2,7 +2,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-
+from django.db.models.base import ObjectDoesNotExist 
 
 #class import
 import machine_usage.lists
@@ -15,6 +15,17 @@ class ForgeUserCreationForm(UserCreationForm):
         self.fields['last_name'].required = True
         self.fields['email'].required = True
 
+    def clean(self, *args, **kwargs):
+        super(ForgeUserCreationForm , self).clean(*args, **kwargs)
+        
+        email = self.cleaned_data.get('email')
+        username = self.cleaned_data.get('username')
+
+        if email  and User.objects.filter(email=email).exclude(username=username).exists():
+            raise forms.ValidationError('Email addresses must be unique.',code='duplicate_email')
+        
+        
+        
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email')
@@ -36,21 +47,33 @@ class ForgeProfileCreationForm(forms.ModelForm):
         self.fields['rin'].required = True
         self.fields['gender'].required = True
         self.fields['major'].required = True
-        
-    class Meta:
-        model = UserProfile 
-        fields = ('rin','gender','major')
+   
     def clean(self, *args , **kwargs):
         super(ForgeProfileCreationForm, self).clean(*args ,**kwargs) 
         
-        if( 'rin' in self.cleaned_data):
-            if(len(self.cleaned_data['rin']) != 9):
-                raise forms.ValidationError("Invalid Rin")
+        rin = self.cleaned_data['rin']
 
+        if(rin):
+            #See if rin is an integer                                                    
             try:
-                int(self.cleaned_data['rin'])
-            except:
-                raise forms.ValidationError("Invalid Rin")
+                int(rin)
+            except ValueError:
+                raise forms.ValidationError("Invalid Rin", code='invalid_rin')
+            
+            #See if rin is correct length
+            if(len(rin) != 9):
+                raise forms.ValidationError("Invalid Rin", code='invalid_rin')
 
+            #See if rin already exists
+            try:
+                UserProfile.objects.get(rin=self.cleaned_data['rin'])
+                raise forms.ValidationError("Rin already taken", code='duplicate_rin')
+            except ObjectDoesNotExist:
+                pass
+
+            
         return self.cleaned_data
-        
+    
+    class Meta:                                 
+        model = UserProfile 
+        fields = ('rin','gender','major')
