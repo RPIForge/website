ec3963147468b0fc43d109cf35f1cb1118c83c96
Michael Honaker <honakm@rpi.edu>
2020-07-28 19:45:22 +0000

    Fixed static css issues

--- forge/settings.py
+++ forge/settings.py
@@ -132,9 +132,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_URL = 'forge/static/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
 
 # Login
 
