1828395217122c827b3da44e87e4a775808ff0f6
Michael Honaker <honakm@rpi.edu>
2020-07-28 22:13:31 +0000

    Updated dev password to match readme

--- forge/settings.py
+++ forge/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'forge_devel',
-        'USER': 'postgresql',
+        'USER': 'postgres',
         'PASSWORD': 'password',
         'HOST': '127.0.0.1',
         'PORT': '5432',
