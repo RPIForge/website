2021bc62903c6cc174113f2b84b88f23ca5d1c82
Michael Honaker <honakm@rpi.edu>
2020-08-27 23:22:14 +0000

    Updated Static Root

--- forge/settings.py
+++ forge/settings.py
@@ -132,6 +132,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Login
 
