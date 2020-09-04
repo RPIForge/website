5d9067932ae4aca8b77d3e3d38268fbf1b468608
Michael Honaker <honakm@rpi.edu>
2020-09-04 03:45:45 +0000

    Update chat box location and functionality.
    I made the grey background and made it so that the chat was more
    reactive. by using percentages and realative positioning.
    
    On a more personal note :'( I dont like css

--- forge/static/forge/js/chat.js
+++ forge/static/forge/js/chat.js
@@ -2,9 +2,16 @@
 
 function toggle_chat(){
 	
-	document.getElementById("chat_contents").style.display = (document.getElementById("chat_contents").style.display == "block" ? "none" : "block");
+	if(document.getElementById("chat_contents").style.display=="block"){
+		document.getElementById("chat_contents").style.display = "none";
+		document.getElementById("chat_footer_tab").style.height = "";
+	} else {
+		document.getElementById("chat_contents").style.display = "block";
+		document.getElementById("chat_footer_tab").style.height = "3%";
+	}
 	console.log(document.getElementById('chat_iframe').src)
 	if(document.getElementById('chat_iframe').src == ''){
 		document.getElementById('chat_iframe').src = chat_url;
 	}	
+	
 }
