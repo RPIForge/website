cf09c9327ddc29b41a2eb7f0518bf257defc75a7
Michael Honaker <mchonaker@gmail.com>
2020-10-27 11:00:00 -0400

    Updated status page to pop out temperature

--- forge/settings.py
+++ forge/settings.py
@@ -66,7 +66,8 @@ TEMPLATES = [
             'django.template.context_processors.request',
             'django.contrib.auth.context_processors.auth',
             'django.contrib.messages.context_processors.messages',
-            'forge.context_processors.channels_url'
+            'forge.context_processors.channels_url',
+            'forge.context_processors.source_url',
             ],
         },
     },
