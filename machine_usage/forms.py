1bc51275135aabbec6e426ec2da9da370156c6c5
Michael Honaker <mchonaker@gmail.com>
2020-11-18 17:46:37 -0500

    Added templates for machine usage and updated form

--- machine_usage/forms.py
+++ machine_usage/forms.py
@@ -7,57 +7,82 @@ from machine_management.models import *
 from formtools.wizard.views import SessionWizardView
 
 
+#Machine Selection form
 class MachineSelectionForm(forms.Form):
     machine = forms.ModelChoiceField(queryset=Machine.objects.filter(in_use=False)) 
 
+#Machine Slot usage
 class MachineSlotUsageForm(forms.Form):
+    #list of slot names and costs
+    slot_name_list = []
+    slot_cost_dict = {}
+    
+    
     def __init__(self, machine, *args, **kwargs):
         super(MachineSlotUsageForm, self).__init__(*args, **kwargs)
         
-        
-        slot_list = []
-        for slot in machine.machine_type.get_slot_names():
+        #for each slot in the machine
+        for slot in machine.machine_type.machineslot_set.all():
+            #get slot name and append to list
+            slot_name = slot.slot_name
+            self.slot_name_list.append(slot_name)
+            
+            
+            #generate list of slot resources
             choice_list = []
-            for resource in slot.allowed_resources:
-                choice_list.append((resource.id, resource.resoruce_name))
+            for resource in slot.allowed_resources.all():
+                choice_list.append((resource.id, resource.resource_name))
+                self.slot_cost_dict[resource.resource_name] = resource.cost_per
             
-            slot_form = forms.ComboField(fields=[forms.ChoiceField(choice_list), forms.DecimalField()])
-            slot_form.label = slot.slot_name
+            #add choice field and decimal field
+            self.fields['{}resource'.format(slot.id)] = forms.ChoiceField(choices=choice_list)
             
-            slot_list.append(slot_form)
-        self.fields['slots'] = forms.ComboField(slot_list)
+            self.fields['{}ammount'.format(slot.id)] = forms.DecimalField()
     
+    
+machine_usage_forms = [
+        ("machine_selection", MachineSelectionForm),
+        ("resource_selection", MachineSlotUsageForm)]
+
+machine_usage_templates = ["formtools/wizard/machine_usage/machine_selection.html","formtools/wizard/machine_usage/resource_selection.html"]
 
 
 class MachineUsageWizard(SessionWizardView):
+    #list form
+    form_list = [MachineSelectionForm, MachineSlotUsageForm]
+        
     def done(self, form_list, **kwargs):
-        print(1)
-        return redirect('/page-to-redirect-to-when-done/')
+        ### REWRITE THIS ###
+        return redirect('/')
     
-    def get_form(self, step=None, data=None, files=None):
-        form = super().get_form(step, data, files)
-        
-        #if step is None:
-        #    step = self.steps.current
-        #
-        #
-        ##try:
-        ##    print(data.get('0-machine'))
-        ##except:
-        ##    print('no data')
-        # 
-        #print(step,step==1,step=='1')
-        #if(step=='1'):
-        #    machine_id = data.get('0-machine')
-        #    machine = Machine.objects.get(id=machine_id)
-        #    form = MachineSlotUsageForm(machine)
-        #    return form
-        
-        
-        return form
-        
+    def get_template_names(self):
+        #display template
+        return [machine_usage_templates[int(self.steps.current)]]
+    
+    
+    #set initial variables for the forms. 
     def get_form_kwargs(self, step):
-        kwargs = super(MachineUsageWizard, self).get_form_kwargs(step)
-        if step == u'1':
-            kwargs['user_id'] = self.request.user.id
-        return kwargs
+        initial = {}
+        
+        print(self.request.POST)
+        print(step)
+        
+        if step == '1':
+            machine_id = self.request.POST.get('0-machine')
+            machine = Machine.objects.get(id=machine_id)
+            initial['machine'] = machine
+
+        return initial    
+    
+    #set template context for forms.
+    def get_context_data(self, form, **kwargs):
+        context = super(MachineUsageWizard, self).get_context_data(form=form, **kwargs)
+        if self.steps.current == '1':
+            context.update({'slot_name_list': form.slot_name_list})
+            context.update({'slot_cost_dict': form.slot_cost_dict})
+            
+        return context
+    
+    
+        
+   
--- machine_usage/urls.py
+++ machine_usage/urls.py
@@ -4,7 +4,7 @@ from . import views
 from .forms import *
 urlpatterns = [
 
-    path('forms/machine_usage', MachineUsageWizard.as_view([MachineSelectionForm, MachineSlotUsageForm]), name='machine_usage'),
+    path('forms/machine_usage', MachineUsageWizard.as_view(), name='machine_usage'),
     path('forms/machine_form', views.generate_machine_form, name='machine_form'),
     path('forms/clear_machine', views.generate_clear_machine_form, name='clear_machine_form'),
     path('forms/failed_usage', views.generate_failed_usage_form, name='failed_usage')
